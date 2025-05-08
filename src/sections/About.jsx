9746d566bd8ca308418c2e5b8f95b41986e90575
Karan Suthar <karansuthar9565@gmail.com>
2025-05-08 13:19:00 +0530

    Update About.jsx

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -2,7 +2,7 @@ import { useRef } from "react";
 import Card from "../components/Card";
 import { Globe } from "../components/globe";
 import CopyEmailButton from "../components/CopyEmailButton";
-import { Frameworks } from "../components/Frameworks";
+import { FrameWorks } from "../components/FrameWorks";
 import { FaInstagram, FaLinkedin, FaGithub, FaEnvelope, FaDiscord } from "react-icons/fa";
 
 const About = () => {
